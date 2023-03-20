a54427627e1c7945832632f018f2dedd80645af7
Viacheslav <slava.barinov2002@yandex.ru>
2023-03-21 02:35:49 +0300

    Done HW2

---
+++ homework_2/oracles.py
@@ -0,0 +1,94 @@
+import numpy as np
+import scipy
+class BaseSmoothOracle(object):
+    """
+    Base class for implementation of oracles.
+    """
+    def func(self, x):
+        """
+        Computes the value of function at point x.
+        """
+        raise NotImplementedError('Func oracle is not implemented.')
+
+    def grad(self, x):
+        """
+        Computes the gradient at point x.
+        """
+        raise NotImplementedError('Grad oracle is not implemented.')
+    
+    def func_directional(self, x, d, alpha):
+        """
+        Computes phi(alpha) = f(x + alpha*d).
+        """
+        return np.squeeze(self.func(x + alpha * d))
+
+    def grad_directional(self, x, d, alpha):
+        """
+        Computes phi'(alpha) = (f(x + alpha*d))'_{alpha}
+        """
+        return np.squeeze(self.grad(x + alpha * d).dot(d))
+
+
+class QuadraticOracle(BaseSmoothOracle):
+    """
+    Oracle for quadratic function:
+       func(x) = 1/2 x^TAx - b^Tx.
+    """
+    
+    def __init__(self, A, b):
+        if not scipy.sparse.isspmatrix_dia(A) and not np.allclose(A, A.T):
+            raise ValueError('A should be a symmetric matrix.')
+        self.A = A
+        self.b = b
+
+    def func(self, x):
+        return 0.5 * x @ self.A @ x - self.b @ x
+
+    def grad(self, x):
+        return self.A @ x - self.b
+
+        
+class LogRegL2Oracle(BaseSmoothOracle):
+    """
+    Oracle for logistic regression with l2 regularization:
+         func(x) = 1/m sum_i log(1 + exp(-b_i * a_i^T x)) + regcoef / 2 ||x||_2^2.
+    Let A and b be parameters of the logistic regression (feature matrix
+    and labels vector respectively).
+    For user-friendly interface use create_log_reg_oracle()
+    Parameters
+    ----------
+        matvec_Ax : function
+            Computes matrix-vector product Ax, where x is a vector of size n.
+        matvec_ATx : function of x
+            Computes matrix-vector product A^Tx, where x is a vector of size m.
+        matmat_ATsA : function
+            Computes matrix-matrix-matrix product A^T * Diag(s) * A,
+    """
+    def __init__(self, matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef):
+        self.matvec_Ax = matvec_Ax
+        self.matvec_ATx = matvec_ATx
+        self.matmat_ATsA = matmat_ATsA
+        self.b = b
+        self.regcoef = regcoef
+
+    def func(self, x):
+        return 1 / len(self.b) * np.sum(np.log(1 + np.exp(-self.b * self.matvec_Ax(x)))) + self.regcoef/2 * x @ x
+
+    def grad(self, x):
+        res = np.divide(self.b, 1 + np.exp(self.b * self.matvec_Ax(x)))
+        res = self.matvec_ATx(res)
+        return res / -len(self.b) + self.regcoef * x
+
+
+def create_log_reg_oracle(A, b, regcoef):
+    """
+    Auxiliary function for creating logistic regression oracles.
+        `oracle_type` must be either 'usual' or 'optimized'
+    """
+    matvec_Ax = lambda x: A @ x 
+    matvec_ATx = lambda x: A.T @ x
+
+    def matmat_ATsA(s):
+        return A.T @ s @ A
+
+    return LogRegL2Oracle(matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef)
